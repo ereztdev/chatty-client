e85c6fe23bd28b9b02497dd32e4190397c4c53ec
erezt.dev <erezt.dev@gmail.com>
2019-04-27 18:38:15 +0300

    added chat Window, need to cleanup

--- src/App.js
+++ src/App.js
@@ -1,34 +1,4 @@
-// import React from 'react';
-// import logo from './logo.svg';
-// import './App.css';
-// import Example from "./WebSocket";
-//
-// function App() {
-//   return (
-//     <div className="App">
-//       <Example/>
-//
-//       <header className="App-header">
-//         <img src={logo} className="App-logo" alt="logo" />
-//         <p>
-//           Edit <code>src/App.js</code> and save to reload.
-//         </p>
-//         <a
-//           className="App-link"
-//           href="https://reactjs.org"
-//           target="_blank"
-//           rel="noopener noreferrer"
-//         >
-//           Learn React
-//         </a>
-//       </header>
-//     </div>
-//   );
-// }
-//
-// export default App;
 import React, {useState, useEffect} from 'react';
-// import logo from './logo.svg';
 import './App.scss';
 import {FontAwesomeIcon} from '@fortawesome/react-fontawesome'
 
@@ -36,37 +6,13 @@ const io = require('socket.io-client');
 const socket = io('http://localhost:3011');
 
 function App() {
-  // const [message, setMessage] = useState({msg: '', room: ''});
 
-  const [messageCount, setMessageCount] = useState({count: 0, room: ''});
+  const [messages, setMessages] = useState([]);
   const [theme, setTheme] = useState('dark');
   const [inRoom, setInRoom] = useState(false);
   const [room, setRoomName] = useState(null);
   const [rooms, setRoomArray] = useState([]);
-  const [user, setUser] = useState({user: ''});
-
-
-  useEffect(() => {
-    setRoomArray(roomArray);
-    socket.on('receive message', (msg) => console.log(msg))
-
-  }, []);
-
-  useEffect(() => {
-    if (room) {
-      socket.emit('room', {room});
-    }
-    socket.on('test', (payload) => {
-      console.log('on receive message');
-      console.log({payload});
-      // setMessageCount(messageCount.count + 1);
-    });
-  });
-
-  useEffect(() => {
-    document.title = `Room: ${room ? room : ''}, Messages: ${messageCount} new messages have been emitted`;
-  }, [messageCount]); //only re-run the effect if new message comes in
-
+  const chatMessagesWindow = document.getElementsByClassName('chat_window--wrapper');
 
   const roomArray = [
     'BTC',
@@ -75,12 +21,28 @@ function App() {
     'XMR',
   ];
 
-  const handleNewMessage = (room) => {
-    console.log('emitting new message');
-    let count = messageCount.count + 1;
-    setMessageCount({count, room});
-    socket.emit('new message', {messageCount});
-  };
+  useEffect(() => {
+    setRoomArray(roomArray);
+    socket.on('RESPONSE_ROOM_MSGS', (data) => {
+      setMessages(data)
+      chatMessagesWindow[0].scrollTop = chatMessagesWindow[0].scrollHeight;
+    });
+  }, []);
+
+  useEffect(() => {
+    chatMessagesWindow.scrollTop = 155;
+    chatMessagesWindow[0].scrollTo(0, chatMessagesWindow.scrollHeight);
+
+    if (room) {
+      socket.emit('room', {room});
+    }
+  }, [room]);
+
+  useEffect(() => {
+    document.title = `Room: ${room ? room : ''}`;
+  });
+
+
 
   const joinRoom = (e) => {
     if (e.target.textContent === room) {
@@ -96,21 +58,27 @@ function App() {
     e.target.style.background = 'red';
     setInRoom(true);
     setRoomName(e.target.textContent);
+    socket.emit('QUERY_ROOM_MSGS', room)
   };
+
   const chatHandler = (e) => {
-    e.preventDefault();
-    e.persist();
-    if (e.innerText === 'Clear') {
-      return e.target.form[0].value = '';
+
+    var textArea = document.getElementsByClassName('chat_window__input')[0];
+    const message = textArea.value;
+
+    if (e) {
+      e.preventDefault();
+      e.persist();
+      if (e.target.innerText === 'Clear') {
+        return textArea = '';
+      }
+      // message = e.target.form[0].value;
     }
-
-    let message = e.target.form[0].value;
-
-    if (e.target.form[0].value !== '') {
+    if (message !== '') {
       socket.emit('new message', {message, room});
       // setMessage({message, room});
     }
-    e.target.form[0].value = '';
+    textArea.value = '';
   };
 
   const roomSpawner = (rooms) => {
@@ -121,7 +89,7 @@ function App() {
 
   const getRoom = () => {
     return room;
-  }
+  };
 
   const themeHandler = () => {
     let newTheme;
@@ -131,6 +99,13 @@ function App() {
     console.log('new theme: ' + newTheme);
     setTheme(newTheme);
   }
+
+  const detectEnter = (e) => {
+    e.persist();
+    if (e.keyCode === 13) {
+      chatHandler()
+    }
+  };
 
   return (
     <div className={`App Theme-${theme}`}>
@@ -143,15 +118,22 @@ function App() {
           {inRoom && `you've entered the ${getRoom()} room`}
           {!inRoom && `Pick a Room to discuss your current trade`}
         </h2>
-
-        <p>{messageCount.count} messages have been emitted</p>
         <div className='rooms--wrapper'>
           {(rooms.length > 0 || rooms.length < 6) ? roomSpawner(rooms) : false}
         </div>
 
+        <div className='chat_window--wrapper'>
+          {messages.length === 0 ? <h1>ChatWindow.js</h1> : false}
+          {messages.map(
+            (value, index) => {
+              return <div key={index} aria-rowcount={index+1} id={index+1} className='chat_window__message'>{value.user.id}: {value.data.message}</div>;
+            })
+          }
+        </div>
         <form>
           <div className="input-group mb-3 chat">
-            <textarea placeholder="Write Something clever.." className="form-control custom-control" rows="3"/>
+            <textarea onKeyDown={detectEnter} placeholder="Write Something clever.."
+                      className="form-control custom-control chat_window__input" rows="3"/>
             <div className="input-group-append">
               <button onClick={chatHandler} className="btn btn-primary" type="submit">Send</button>
               <button onClick={chatHandler} className="btn btn-danger" type="button">Clear</button>
